35f42a4566e137529b93ff19eb36793f245c6db5
tlietz <theodore.lietz@gmail.com>
2022-04-15 18:05:08 -0500

    can edit the content of list items

--- lib/collaborlist_web/controllers/collab_controller.ex
+++ lib/collaborlist_web/controllers/collab_controller.ex
@@ -18,8 +18,8 @@ defmodule CollaborlistWeb.CollabController do
     render(conn, "new.html", changeset: changeset, list: list)
   end
 
-  def create(conn, %{"list_id" => list_id, "list_item" => item}) do
-    case List.create_list_item(item, Catalog.get_list!(list_id)) do
+  def create(conn, %{"list_id" => list_id, "list_item" => item_params}) do
+    case List.create_list_item(item_params, Catalog.get_list!(list_id)) do
       {:ok, _item} ->
         conn
         |> put_flash(:info, "Item added successfully.")
@@ -27,6 +27,28 @@ defmodule CollaborlistWeb.CollabController do
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def edit(conn, %{"list_id" => list_id, "id" => item_id}) do
+    list = Catalog.get_list!(list_id)
+    item = List.get_list_item!(item_id)
+    changeset = List.change_list_item(item)
+    render(conn, "edit.html", list: list, list_item: item, changeset: changeset)
+  end
+
+  def update(conn, %{"list_id" => list_id, "id" => item_id, "list_item" => item_params}) do
+    list = Catalog.get_list!(list_id)
+    list_item = List.get_list_item!(item_id)
+
+    case List.update_list_item(list_item, item_params) do
+      {:ok, _list_item} ->
+        conn
+        |> put_flash(:info, "List updated successfully.")
+        |> redirect(to: Routes.collab_path(conn, :index, list_id))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", list: list, changeset: changeset)
     end
   end
 
