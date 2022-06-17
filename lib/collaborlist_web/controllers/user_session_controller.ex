90f2c3c36e5a7cfb2817435eb3719082698a0c27
tlietz <theodore.lietz@gmail.com>
2022-06-17 10:32:01 -0500

    hide error message from user

--- lib/collaborlist_web/controllers/user_session_controller.ex
+++ lib/collaborlist_web/controllers/user_session_controller.ex
@@ -16,9 +16,9 @@ defmodule CollaborlistWeb.UserSessionController do
         # this has the `external` tag because the `referer` from `get_req_header` returns a full URL.
         |> redirect(external: referer)
 
-      {:error, reason} ->
+      {:error, _reason} ->
         conn
-        |> put_flash(:error, reason)
+        |> put_flash(:error, "Sign in failed, please try again")
         |> redirect(to: Routes.list_path(conn, :index))
     end
   end
