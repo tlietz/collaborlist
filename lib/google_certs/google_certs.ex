6bf33d16d859732edc487c46ff1babca13256c85
tlietz <theodore.lietz@gmail.com>
2022-06-25 08:50:13 -0500

    reorganize google certs directory structure

--- lib/google_certs/google_certs.ex
+++ lib/google_certs/google_certs.ex
@@ -18,23 +18,28 @@ defmodule GoogleCerts do
     "email" => "",
     "email_verified" => true,
     "exp" => 1655479451,
-    "family_name" => "Lietz",
-    "given_name" => "Ted",
+    "family_name" => "Last name",
+    "given_name" => "First name",
     "iat" => 1655475851,
     "iss" => "https://accounts.google.com",
     "jti" => "40c49e3b0bd44a02ab646122b9f7420754ff3045",
     "name" => "Ted Lietz",
     "nbf" => 1655475551,
-    "picture" => "https://lh3.googleusercontent.com/a-/AOh14GiIQRM-24G9H6Dx6wBc-AltUDYXWz3GLOnLv_VL0A=s96-c",
-    "sub" => "113476586568097370311"
+    "picture" => "link to picture",
+    "sub" => "unique user id"
   }
 
   The `sub` field is unique to each user and can be used to identify the authenticated user.
   """
 
   # Client facing functions
+
+  @doc """
+  Returns an `id_token` of the user that was authenticated.
+  An `id_token` is what Google calls their JWT.
+  """
   @spec user_id_token(conn :: any, params :: any) ::
-          {:error, any} | {:ok, jwt :: map}
+          {:error, any} | {:ok, id_token :: map}
   def user_id_token(conn, params) do
     with {:ok, _token} <- GoogleCerts.CSRF.verify_csrf_token(conn, params),
          {:ok, jwt} <- GoogleCerts.Authentication.verify_id_token(conn, params) do
@@ -43,5 +48,8 @@ defmodule GoogleCerts do
       {:error, reason} ->
         {:error, reason}
     end
+  end
+
+  def uid(id_token) do
   end
 end
