d203b62ff32057ed50dd25c8311a911ef983cb5e
tlietz <theodore.lietz@gmail.com>
2022-06-29 11:00:16 -0500

    google sign in works even when user already registered with an email

--- lib/collaborlist_web/controllers/google_user_controller.ex
+++ lib/collaborlist_web/controllers/google_user_controller.ex
@@ -12,7 +12,8 @@ defmodule CollaborlistWeb.GoogleUserController do
           conn
           |> get_req_header("referer")
 
-        # TODO: If there is already an email associated with an account, but a Google sign-in was used, popup and ask if user wants to use Google as default, or use both.
+        # TODO: If there is already an email associated with an account, but a Google sign-in was used, sign them into the corresponding email, and insert the google_uid.
+        # TODO: Automatically confirm an email if google sign-in is used
         # TODO: Auto logout feature for google account in settings
 
         google_user = id_token |> GoogleCerts.uid() |> Account.get_user_by_google_uid()
@@ -23,19 +24,30 @@ defmodule CollaborlistWeb.GoogleUserController do
           # this has the `external` tag because the `referer` from `get_req_header` returns a full URL.
           |> redirect(external: referer)
         else
-          {:ok, new_google_user} =
-            Account.register_user(%{
-              email: id_token |> GoogleCerts.email(),
-              google_uid: id_token |> GoogleCerts.uid()
-            })
+          # user is already registered with this email without a google account
+          maybe_user = id_token |> GoogleCerts.email() |> Account.get_user_by_email()
 
-          conn
-          |> UserAuth.log_in_user(new_google_user)
-          |> put_flash(
-            :info,
-            "Registered with new Google account"
-          )
-          |> redirect(external: referer)
+          if maybe_user do
+            maybe_user
+            |> IO.inspect(label: "USER")
+
+            conn
+            |> UserAuth.log_in_user(maybe_user)
+          else
+            {:ok, new_google_user} =
+              Account.register_user(%{
+                email: id_token |> GoogleCerts.email(),
+                google_uid: id_token |> GoogleCerts.uid()
+              })
+
+            conn
+            |> UserAuth.log_in_user(new_google_user)
+            |> put_flash(
+              :info,
+              "Registered with new Google account"
+            )
+            |> redirect(external: referer)
+          end
         end
 
       {:error, reason} ->
