a6e90921d8df3761b04ea75f97871c534896949a
tlietz <theodore.lietz@gmail.com>
2022-07-20 10:54:44 -0500

    add timestamps

--- priv/repo/migrations/20220720154355_create_invites.exs
+++ priv/repo/migrations/20220720154355_create_invites.exs
@@ -6,6 +6,8 @@ defmodule Collaborlist.Repo.Migrations.CreateInvites do
       add :uuid, :uuid, primary_key: true
 
       add :list_id, references(:lists, on_delete: :delete_all)
+
+      timestamps()
     end
 
     create index(:users, [:list_id])
