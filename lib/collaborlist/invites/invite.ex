74df35e3150245254066faeb900d2cb889e6025a
tlietz <theodore.lietz@gmail.com>
2022-07-20 12:38:03 -0500

    customize invites schema

--- lib/collaborlist/invites/invite.ex
+++ lib/collaborlist/invites/invite.ex
@@ -2,7 +2,7 @@ defmodule Collaborlist.Invites.Invite do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @primary_key {:uuid, :binary_id, autogenerate: true}
+  @primary_key {:invite_code, :binary_id, autogenerate: true}
 
   schema "users" do
     belongs_to :list, Collaborlist.Catalog.List
@@ -11,9 +11,9 @@ defmodule Collaborlist.Invites.Invite do
   end
 
   @doc false
-  def changeset(user, attrs) do
-    user
-    |> cast(attrs, [:name])
-    |> validate_required([:name])
+  def changeset(invite, attrs) do
+    invite
+    |> cast(attrs, [:invite_code])
+    |> validate_required([:invite_code])
   end
 end
