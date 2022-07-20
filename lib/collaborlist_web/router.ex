7ae26bf87ba76ace59073efc955f203147128408
tlietz <theodore.lietz@gmail.com>
2022-07-20 12:52:33 -0500

    remove unused resource

--- lib/collaborlist_web/router.ex
+++ lib/collaborlist_web/router.ex
@@ -39,7 +39,6 @@ defmodule CollaborlistWeb.Router do
   scope "/", CollaborlistWeb do
     pipe_through [:browser, :require_authenticated_user, :require_user_list_collaborator]
 
-    resources "/", ListController, except: [:index, :new, :create, :edit, :update, :delete, :show]
     get "/:list_id", ListController, :edit
     put "/:list_id", ListController, :update
     delete "/:list_id", ListController, :delete
