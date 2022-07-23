d3aee6f8cf243bed4a0beba88545dbc1f50bd44f
tlietz <theodore.lietz@gmail.com>
2022-07-23 10:22:33 -0500

    render prod or dev links depending on mix env

--- lib/collaborlist_web/views/invites_view.ex
+++ lib/collaborlist_web/views/invites_view.ex
@@ -1,7 +1,22 @@
 defmodule CollaborlistWeb.InvitesView do
   use CollaborlistWeb, :view
 
+  @dev_link "http://localhost:4000/lists/"
+  @prod_link "https://www.collaborlist.com/lists/"
+
   def invite_link(invite) do
-    invite.invite_code
+    if Mix.env() == :prod do
+      prod_link(invite)
+    else
+      dev_link(invite)
+    end
+  end
+
+  defp dev_link(invite) do
+    "#{@dev_link}#{invite.list_id}/invite/#{invite.invite_code}"
+  end
+
+  defp prod_link(invite) do
+    "#{@prod_link}#{invite.list_id}/invite/#{invite.invite_code}"
   end
 end
