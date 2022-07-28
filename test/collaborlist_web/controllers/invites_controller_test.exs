570b7b91ffa491a4d9530cf841cd9dc092db6c11
tlietz <theodore.lietz@gmail.com>
2022-07-28 13:07:07 -0500

    finishes tests of invites controller index

--- test/collaborlist_web/controllers/invites_controller_test.exs
+++ test/collaborlist_web/controllers/invites_controller_test.exs
@@ -2,6 +2,8 @@ defmodule CollaborlistWeb.InvitesControllerTest do
   use CollaborlistWeb.ConnCase
 
   import Collaborlist.CatalogFixtures
+  import Collaborlist.AccountFixtures
+  import Collaborlist.InvitesFixtures
 
   alias CollaborlistWeb.UserAuth
   alias Collaborlist.Catalog
@@ -10,4 +12,36 @@ defmodule CollaborlistWeb.InvitesControllerTest do
   @create_attrs %{title: "some title"}
   @update_attrs %{title: "some updated title"}
   @invalid_attrs %{title: 4, checked: "foo", striked: "bar"}
+
+  setup :register_and_log_in_user
+
+  describe "index" do
+    test "lists all invites that belongs to a user", %{conn: conn} do
+      user = user_fixture()
+      list = list_fixture(%{}, user)
+
+      invite = invite_fixture(user, list)
+
+      conn =
+        log_in_user(conn, user)
+        |> UserAuth.fetch_current_user(%{})
+        |> get(Routes.invites_path(conn, :index, list.id))
+
+      assert html_response(conn, 200) =~ ~s/#{invite.invite_code}/
+    end
+
+    test "redirects if user is not a collaborator on the list", %{conn: conn} do
+      user = user_fixture()
+      list = list_fixture()
+
+      invite = invite_fixture(user, list)
+
+      conn =
+        log_in_user(conn, user)
+        |> UserAuth.fetch_current_user(%{})
+        |> get(Routes.invites_path(conn, :index, list.id))
+
+      assert html_response(conn, 302)
+    end
+  end
 end
