7d00ed5ef629554cff295dea8f7dbe742d36a371
tlietz <theodore.lietz@gmail.com>
2022-07-29 10:22:04 -0500

    adds testcase for creating invite code in controller

--- test/collaborlist_web/controllers/invites_controller_test.exs
+++ test/collaborlist_web/controllers/invites_controller_test.exs
@@ -44,4 +44,25 @@ defmodule CollaborlistWeb.InvitesControllerTest do
       assert html_response(conn, 302)
     end
   end
+
+  describe "create invite" do
+    test "redirects to invites index of a list collaborator when data is valid", %{conn: conn} do
+      user = user_fixture()
+      list = list_fixture(%{}, user)
+
+      conn =
+        log_in_user(conn, user)
+        |> UserAuth.fetch_current_user(%{})
+        |> post(Routes.invites_path(conn, :create, list.id))
+
+      assert %{list_id: id} = redirected_params(conn)
+
+      assert redirected_to(conn) == Routes.invites_path(conn, :index, id)
+
+      [invite | _] = Invites.list_invites(id)
+
+      conn = get(conn, Routes.invites_path(conn, :index, id))
+      assert html_response(conn, 200) =~ invite.invite_code
+    end
+  end
 end
