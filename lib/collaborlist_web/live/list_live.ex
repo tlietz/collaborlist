4164e8432e1231125f69152227eef2bb5b2b6374
tlietz <theodore.lietz@gmail.com>
2022-08-14 15:26:12 -0500

    add todo

--- lib/collaborlist_web/live/list_live.ex
+++ lib/collaborlist_web/live/list_live.ex
@@ -3,15 +3,18 @@ defmodule CollaborlistWeb.ListLive do
 
   alias Collaborlist.Catalog
 
-  # TODO: get lists in `mount/3` since that is only run once per liveview, then any updates to the user's lists
-
   on_mount {CollaborlistWeb.UserAuth, :current_user}
 
   def mount(_params, _session, socket) do
     user = socket.assigns[:current_user]
     lists = Catalog.list_lists(user)
 
-    {:ok, assign(socket, :lists, lists)}
+    changeset = Catalog.change_list(%Catalog.List{})
+
+    {:ok,
+     socket
+     |> assign(:lists, lists)
+     |> assign(:changeset, changeset)}
   end
 
   def handle_event("inc", _, socket) do
