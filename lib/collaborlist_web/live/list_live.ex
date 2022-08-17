21368bfa40c6fdc3823948056e1cf8c0916a5648
tlietz <theodore.lietz@gmail.com>
2022-08-17 16:21:25 -0500

    realtime updating to top of page when creating lists

--- lib/collaborlist_web/live/list_live.ex
+++ lib/collaborlist_web/live/list_live.ex
@@ -16,16 +16,11 @@ defmodule CollaborlistWeb.ListLive do
     {:ok,
      socket
      |> assign(:lists, lists)
-     |> assign(:changeset, changeset)
-     |> assign(:val, 0)}
+     |> assign(:changeset, changeset)}
   end
 
   def handle_event("validate", _, socket) do
     {:noreply, socket}
-  end
-
-  def handle_event("dec", _, socket) do
-    {:noreply, assign(socket, val: socket.assigns.val - 1)}
   end
 
   def handle_event("save", %{"list" => list_params}, socket) do
@@ -33,9 +28,6 @@ defmodule CollaborlistWeb.ListLive do
 
     case Catalog.create_list(user, list_params) do
       {:ok, list} ->
-        socket
-        |> IO.inspect(label: "BEFORE")
-
         {:noreply, assign(socket, lists: [list | socket.assigns.lists])}
 
       {:error, %Ecto.Changeset{} = changeset} ->
@@ -43,69 +35,9 @@ defmodule CollaborlistWeb.ListLive do
          socket
          |> assign(:changeset, changeset)}
     end
-
-    {:noreply, socket}
   end
 
   def render(assigns) do
-    ~H"""
-    <h1>The count is: <%= @val %></h1>
-    <button phx-click="dec">-</button>
-
-    <h1>Listing Lists</h1>
-
-    <span>
-      <b>Create New List:</b>
-
-      <.form let={f} for={@changeset} phx-change="validate" phx-submit="save">
-        <%= if @changeset.action do %>
-          <div class="alert alert-danger">
-            <p>
-              Oops, something went wrong!
-            </p>
-          </div>
-        <% end %>
-
-        <%= label(f, :title) %>
-        <%= text_input(f, :title) %>
-        <%= error_tag(f, :title) %>
-
-        <button type="submit">
-          Save
-        </button>
-      </.form>
-    </span>
-    <table>
-      <thead>
-        <tr>
-          <th>List</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <%= for list <- @lists do %>
-          <tr>
-            <td><%= list.title %></td>
-
-            <td>
-              <span><%= link("Collab", to: Routes.collab_path(@socket, :index, list.id)) %></span>
-              <br />
-              <span>
-                <%= link("Change List Name", to: Routes.list_path(@socket, :edit, list.id)) %>
-              </span>
-              <br />
-              <span>
-                <%= link("Delete",
-                  to: Routes.list_path(@socket, :delete, list.id),
-                  method: :delete,
-                  data: [confirm: "Are you sure?"]
-                ) %>
-              </span>
-            </td>
-          </tr>
-        <% end %>
-      </tbody>
-    </table>
-    """
+    Phoenix.View.render(CollaborlistWeb.ListView, "index.html", assigns)
   end
 end
