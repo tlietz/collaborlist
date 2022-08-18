ba71fa5b7836564f09be86bcde7c2c1b3b100f17
tlietz <theodore.lietz@gmail.com>
2022-08-18 09:08:04 -0500

    all users see list updates in realtime

--- lib/collaborlist_web/live/collab_live.ex
+++ lib/collaborlist_web/live/collab_live.ex
@@ -6,9 +6,13 @@ defmodule CollaborlistWeb.CollabLive do
   alias Collaborlist.List.ListItem
   alias Collaborlist.List
 
+  alias Phoenix.PubSub
+
   on_mount {CollaborlistWeb.UserAuth, :current_user}
 
   def mount(%{"list_id" => list_id}, _session, socket) do
+    PubSub.subscribe(Collaborlist.PubSub, list_id)
+
     list = Catalog.get_list!(list_id)
     list_items = List.list_list_items(list_id)
 
@@ -26,13 +30,25 @@ defmodule CollaborlistWeb.CollabLive do
   end
 
   def handle_event("save", %{"list_item" => item_params}, socket) do
-    case List.create_list_item(item_params, socket.assigns.list) do
+    current_list = socket.assigns.list
+
+    case List.create_list_item(item_params, current_list) do
       {:ok, item} ->
-        {:noreply, assign(socket, list_items: [item | socket.assigns.list_items])}
+        new_state = assign(socket, list_items: [item | socket.assigns.list_items])
+        CollaborlistWeb.Endpoint.broadcast_from(self(), topic(socket), "save", new_state.assigns)
+        {:noreply, new_state}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, :changeset, changeset)}
     end
+  end
+
+  defp topic(socket) do
+    Integer.to_string(socket.assigns.list.id)
+  end
+
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, list_items: msg.payload.list_items)}
   end
 
   def render(assigns) do
