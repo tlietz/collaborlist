528f2523e1117ae4dff6226259bad4b9355d8564
tlietz <theodore.lietz@gmail.com>
2022-08-19 06:47:27 -0500

    remove corresponding todo

--- lib/collaborlist_web/live/collab_live.ex
+++ lib/collaborlist_web/live/collab_live.ex
@@ -1,4 +1,3 @@
-# TODO: find a way to clear the `error_tag` in live_new_item_form.html.heex upon submitting a new list_item
 defmodule CollaborlistWeb.CollabLive do
   use CollaborlistWeb, :live_view
 
