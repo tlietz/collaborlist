5e94f1af9418ed2f8209111c34127383862835eb
tlietz <theodore.lietz@gmail.com>
2022-08-19 09:18:08 -0500

    change root url that some functions use based on environment(dev, prod, etc.)

---
+++ lib/helpers.ex
@@ -0,0 +1,12 @@
+defmodule Collaborlist.Helpers do
+  @dev_link "http://localhost:4000/"
+  @prod_link "https://www.collaborlist.com/"
+
+  def root_url() do
+    if Mix.env() == :prod do
+      @prod_link
+    else
+      @dev_link
+    end
+  end
+end
